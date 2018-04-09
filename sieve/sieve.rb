de08e45379feff3d3ad65a0c329b1bc9ff6e970b
“Ashwin <ashwinvidiyala@gmail.com>
2018-04-09 14:53:45 -0500

    Get Roman Numerals exercise

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -1,5 +1,18 @@
 class Sieve
-  def intialize number
-    @number = number
+  def initialize limit
+    @limit = limit
+  end
+
+  def primes all_numbers = [], current_prime = 2
+    all_numbers = (2..@limit).to_a if all_numbers = []
+    all_numbers.map.with_index do |n, i|
+      if n % current_prime == 0 && n != current_prime
+        n = 0
+      else
+        current_prime = all_numbers[i + 1]
+        primes all_numbers, current_prime
+      end
+    end
+    all_numbers
   end
 end
