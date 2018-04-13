2b5c044821ec55c15cc3d17a89f299805e36a0e9
“Ashwin <ashwinvidiyala@gmail.com>
2018-04-13 17:56:29 -0500

    Finish leap year assignment

---
+++ leap/leap.rb
@@ -0,0 +1,10 @@
+class Year
+  def self.leap? year
+    (year % 4).zero? && (year % 100).nonzero? ||
+    (year % 100).zero? && (year % 400).zero?
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
